620f5fbe3e53ccf6dbc99975b94601ea35e95dbc
Dmitry Sychev <axiumone@gmail.com>
2020-06-01 18:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,19 +22,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 
 function findBy(array, findFn) {
-<<<<<<< HEAD
   for(var i = 0; i < array.length; i++){
     if(findFn(array[i]))
       return array[i]
-=======
-  var findFn = "";
-  for(var i = 0; i < array.length; i++){
-    if(array[i] === findFn) {
-      findFn = array[i];
-      return findFn
-    } else {
-      // return findFn = null;
     }
->>>>>>> 9b84a3e1b6a266d14ed62f84414407a21776e623
   }
 }
